ae23a82118c45203fe255d142f9a5977c283b87b
daredevil0905 <sanchetng@gmail.com>
2023-09-30 20:06:07 +0530

    Add changes

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
       <Header setCoordinates={setCoordinates} />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List places={filteredPlaces.length ? filteredPlaces : places} childClicked={childClicked} isLoading={isLoading} type={type} setType={setType} rating={rating} setRating={setRating} />
+          <List places={filteredPlaces?.length ? filteredPlaces : places} childClicked={childClicked} isLoading={isLoading} type={type} setType={setType} rating={rating} setRating={setRating} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,10 @@ export const getPlacesData = async (type, sw, ne) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
+        bl_latitude: sw?.lat,
+        tr_latitude: ne?.lat,
+        bl_longitude: sw?.lng,
+        tr_longitude: ne?.lng,
       },
       headers: {
         'X-RapidAPI-Key': process.env.REACT_APP_TRAVEL_API_KEY,
